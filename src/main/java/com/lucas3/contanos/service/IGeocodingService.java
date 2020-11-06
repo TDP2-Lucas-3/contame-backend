a7ba8397d07bc4ddac6f75bdf9faa07c57769cb4
matiasReimondo <matiasreimondo@gmail.com>
2020-11-06 09:51:24 -0300

    Fix de geolocalizacion

--- src/main/java/com/lucas3/contanos/service/IGeocodingService.java
+++ src/main/java/com/lucas3/contanos/service/IGeocodingService.java
@@ -1,8 +1,9 @@
 package com.lucas3.contanos.service;
 
 import com.lucas3.contanos.model.exception.FailedReverseGeocodeException;
+import com.lucas3.contanos.model.response.geocoding.LocationResponse;
 
 public interface IGeocodingService {
 
-    String getLocationFromCoordinates(Double latitude, Double longitude) throws FailedReverseGeocodeException;
+    LocationResponse getLocationFromCoordinates(Double latitude, Double longitude) throws FailedReverseGeocodeException;
 }
