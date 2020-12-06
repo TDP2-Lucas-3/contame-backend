0d5286588254b413885b673dab01c4ab2c5d89a7
matiasReimondo <matiasreimondo@gmail.com>
2020-12-06 14:41:09 -0300

    Otro fix

--- src/main/java/com/lucas3/contanos/model/filters/DataFilter.java
+++ src/main/java/com/lucas3/contanos/model/filters/DataFilter.java
@@ -1,13 +1,44 @@
 package com.lucas3.contanos.model.filters;
 
-import java.util.Date;
 
 public class DataFilter {
 
     private String hood;
 
-    private Date createDate;
+    private String createDate;
 
-    private Date completeDate;
+    private String completeDate;
 
+    public DataFilter() {
+    }
+
+    public DataFilter(String hood, String createDate, String completeDate) {
+        this.hood = hood;
+        this.createDate = createDate;
+        this.completeDate = completeDate;
+    }
+
+    public String getHood() {
+        return hood;
+    }
+
+    public void setHood(String hood) {
+        this.hood = hood;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCompleteDate() {
+        return completeDate;
+    }
+
+    public void setCompleteDate(String completeDate) {
+        this.completeDate = completeDate;
+    }
 }
